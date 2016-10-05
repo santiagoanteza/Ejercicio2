ac598766031a4b20e9f052553e1943af4ee98c91
santiagoanteza <santiagoanteza@gmail.com>
2016-10-05 14:29:08 -0400

    ejercicio2

---
+++ src/ejercicio2/Ejercicio2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Vera AA
+ */
+public class Ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    int[ ] arregloVariable = new int[35]; int valorVariable; valorVariable = 24;
+    arregloVariable[valorVariable] = 53;
+    System.out.println("El numero de posicion es"+valorVariable+"en el arreglo"+arregloVariable[valorVariable]);
+    }
+}
